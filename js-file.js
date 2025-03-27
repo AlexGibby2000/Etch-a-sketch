1c3025e9a701a9b874a3ce2ba6926fa4743b73b6
AlexGibby2000 <42209825+AlexGibby2000@users.noreply.github.com>
2025-03-27 14:40:05 -0400

    created sketch pad that can be doodled on with hovering of mouse

---
+++ js-file.js
@@ -0,0 +1,18 @@
+function makeGrid(size){
+    let container = document.querySelector("#sketch-screen");
+    for(let i=0; i<size;i++){
+        let column = document.createElement("div");
+        column.classList.add("column");
+        for(let j=1; j<=size;j++){
+            const row= document.createElement("div");
+            row.classList.add("row");
+            row.addEventListener("mouseover",()=>{
+                row.style.backgroundColor="black";
+            });
+            column.appendChild(row);
+        }
+        container.appendChild(column);
+    }
+}
+
+makeGrid(100);
